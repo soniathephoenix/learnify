2539b64e5c19bd57ed7caf8af079466eb19e4eb2
allredmatt <matt@allred.digital>
2025-02-05 10:56:06 +0000

    Add next button logic

--- frontend/src/questions.js
+++ frontend/src/questions.js
@@ -1,5 +1,8 @@
 const questionP = document.getElementById('question')
 const answerContainer = document.getElementById('answer-container')
+const nextBtn = document.getElementById('next')
+
+var question_id
 
 fetch(`https://learnifybackend-wvnw.onrender.com/users/currentq/`, {
     headers: {
@@ -8,7 +11,7 @@ fetch(`https://learnifybackend-wvnw.onrender.com/users/currentq/`, {
 })
 .then((data) => data.json())
 .then((response) => {
-
+    question_id = data.question_id
     questionP.innerHTML = response.question
 
     answerContainer.children[0].innerHTML = answerContainer.children[0].innerHTML + response.option_a
@@ -16,3 +19,31 @@ fetch(`https://learnifybackend-wvnw.onrender.com/users/currentq/`, {
     answerContainer.children[2].innerHTML = answerContainer.children[2].innerHTML + response.option_c
     answerContainer.children[3].innerHTML = answerContainer.children[3].innerHTML + response.option_d
 })
+
+const submitAnswer = () => {
+    const checkRadio = document.querySelector('input[name="answer"]:checked');
+    if(checkRadio === undefined) {
+        return
+    } else{
+        fetch(`https://learnifybackend-wvnw.onrender.com/questions/${question_id}/correct`)
+        .then(data => data.json())
+        .then(questionAnswer => {
+            console.log("Q ans: ", questionAnswer)
+            console.log("Check Radio: ", checkRadio.textContent)
+            if(checkRadio === questionAnswer){
+
+            }
+            /*
+            fetch(`https://learnifybackend-wvnw.onrender.com/update-points`, {
+                method: "POST", //GET??
+                headers: {
+                    authorization: localStorage.getItem("token"),
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+            })*/
+        })
+    }
+}
+
+addEventListener('click', submitAnswer)
