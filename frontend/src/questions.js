7538060f848fd9199f67b9163108ab1f653314a6
Matt Allred <52718473+allredmatt@users.noreply.github.com>
2025-02-06 18:39:09 +0000

    Update questions.js

--- frontend/src/questions.js
+++ frontend/src/questions.js
@@ -69,7 +69,7 @@ function loadAnswers(data) {
 
 function loadImages(clue_url) {
     imageTag.src = clue_url
-    imageTag.hidden = true
+    modal.style.display = "none";
 }
 
 function startTimer() {
@@ -164,5 +164,25 @@ async function resetPoints() {
 }
 
 function displayImageTag(){
-    imageTag.hidden = false
+    openModal()
+}
+
+
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+
+function openModal() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
